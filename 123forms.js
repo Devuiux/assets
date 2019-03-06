706d9d8e02658841887ed484146e0bd45a83f54d
Catalin Costinas <costinas.catalin@gmail.com>
2019-03-06 21:55:42 +0200

    Create 123forms.js

---
+++ 123forms.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function() {
+    var url = (window.location != window.parent.location) ? document.referrer : document.location.href;
+
+    console.log('ttestt', url);
+  });
+  
